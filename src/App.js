28367f3817aef30d7b74eee795b91f8702f2e35f
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-07 13:07:18 +0530

    Design and answers displayed and continously

--- src/App.js
+++ src/App.js
@@ -130,3 +130,67 @@ function App() {
 }
 
 export default App;
+
+// function App() {
+//   const [message, setMessage] = useState("");
+//   const [responses, setResponses] = useState([]);
+//   const responseContainerRef = useRef(null);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     fetch("http://localhost:3001/", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ message }),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const newResponse = {
+//           question: message,
+//           answer: data.message.content,
+//         };
+//         setResponses([...responses, newResponse]);
+//       })
+//       .catch((error) => console.log(error));
+//     setMessage("");
+//   };
+
+//   // Scroll to the bottom of the response container when new responses are added
+//   useEffect(() => {
+//     if (responseContainerRef.current) {
+//       responseContainerRef.current.scrollTop =
+//         responseContainerRef.current.scrollHeight;
+//     }
+//   }, [responses]);
+
+//   return (
+//     <div className="App">
+//       <div className="response-container" ref={responseContainerRef}>
+//         {responses.map((response, index) => (
+//           <div key={index} className="response">
+//             <div className="code">{"<div>"}</div>
+//             <div className="code-indent">{'<div className="answer">'}</div>
+//             <div className="code-indent">{response.answer}</div>
+//             <div className="code-indent">{"</div>"}</div>
+//             <div className="code-indent">{'<div className="question">'}</div>
+//             <div className="code-indent">{response.question}</div>
+//             <div className="code-indent">{"</div>"}</div>
+//             <div className="code">{"</div>"}</div>
+//           </div>
+//         ))}
+//       </div>
+//       <form onSubmit={handleSubmit} className="input-form">
+//         <textarea
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//           placeholder="Type your message..."
+//         ></textarea>
+//         <button type="submit">Send</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
